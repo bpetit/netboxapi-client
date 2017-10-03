a4f96814590607ba82fca5838e3b6ac2c79e2e0d
Benoit Petit <bpetit@0rk.in>
2017-10-03 14:28:42 +0200

    This branch will be python 2 version for now.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='netboxapi-client',
+    name='netboxapi_client',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
@@ -74,7 +74,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[],
 
-    python_requires='>=3',
+    #python_requires='>=3',
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
